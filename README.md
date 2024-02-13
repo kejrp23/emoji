c0dbdccbb1114714b44fb902e8e58a6b111c6990
Kejrp23 <j.pittman51@att.net>
2024-02-12 22:33:21 -0500

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# emoji
+# Emoji Charaters
+
+
+- This header file is work in progress. 
+- It will contain as many emojis as I can convert. 
+- I will add to this list as often as I can. 
---
+++ emoji.h
@@ -0,0 +1,15 @@
+//Created by Jason R. Pittman 2/12/2024
+
+#include <iostream>
+
+
+
+
+namespace emoji {
+
+		std::string smilie = "\U0001f606";
+		std::string crying = "\U0001F62D";
+		std::string lol = "\U0001F923";
+		std::string skull = "\U0001F480";
+		std::string facepalm = "\U0001F926";
+};
